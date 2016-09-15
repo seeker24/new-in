0ca7824b06a42bfcfe4c7af1529fcf27e5e3ecc2
seeker24 <golsarchat@chmail.ir>
2016-09-15 16:28:53 +0430

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 ob_start();
-define('API_KEY','XXXX');
+define('API_KEY','218693729:AAEIb-WpoVonEl9OTxVBP7SKtS6YxLiw1cQ');
 $the_admin_id = 0;
 
 file_put_contents("count",file_get_contents("count")+1);
